a56cacf76291ddaa5a1ce7161673b7466dc9cdce
Prathameshp98 <prathameshpatil2812@gmail.com>
2022-02-22 00:59:22 +0530

    added hashing and rectified login errors

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-const e = require("express");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -21,10 +23,10 @@ app.use(express.static("public"));
 mongoose.connect("mongodb+srv://admin_prathamesh:1210Nanu@cluster0.hm590.mongodb.net/blogDB");
 
 //create post sechema
-const postSchema = {
+const postSchema = mongoose.Schema({
     name: String,
     body: String
-};
+});
 
 //create a model/collection for posts
 const Post = mongoose.model("Post",postSchema);
@@ -36,25 +38,17 @@ const samplePost = new Post({
 });
 
 //create userData schema
-const userDataSchema = {
+const userDataSchema = mongoose.Schema({
     email: String,
     password: String,
     isLogin: Boolean
-};
+});
 
 //create model/collection for userData
 const userData = mongoose.model("userData",userDataSchema);
 
 
 app.get("/",function(req,res){
-
-    userData.findOneAndUpdate({},{isLogin: false},function(err,data){
-        if(err){
-            console.log(err);
-        } else {
-            console.log(data);
-        }
-    });
 
     Post.find({},function(err,foundItems){
         res.render("home",{
@@ -100,13 +94,24 @@ app.post("/register_page",function(req,res){
         if(foundEmail){
             res.redirect("/userExists");
         } else {
-            const newUser = new userData({
-                email: email,
-                password: password,
-                isLogin: false
+
+            bcrypt.hash(password,saltRounds,function(err,hash){
+                const newUser = new userData({
+                    email: email,
+                    password: hash
+                });
+
+                newUser.save(function(err){
+                    if(err){
+                        console.log(err);
+                    } else {
+                        res.redirect("/login_page");
+                    }
+                });
+                
             });
-            newUser.save();
-            res.redirect("/login_page");
+
+            
         }
 
     });
@@ -134,6 +139,118 @@ app.get("/incorrectPass",function(req,res){
     });
 });
 
+
+app.get("/login_home",function(req,res){
+
+    const fullUser = req.query.fullUser;
+    const arr = fullUser.split(" ");
+    const userName = (arr[0]);
+
+    Post.find({},function(err,foundItems){
+        res.render("login_home",{
+            fullUser: fullUser,
+            userName: userName,
+            homeContent: homeStartingContent,
+            contents: foundItems
+        });
+
+    });
+
+    
+    
+    
+});
+
+app.post("/login_home",function(req,res){
+
+    const email = _.lowerCase(req.body.emailInput);
+    const password = req.body.passwordInput;
+
+    userData.findOne({email: email},function(err,foundUser){
+        if(foundUser){      
+            
+            bcrypt.compare(password,foundUser.password,function(err,result){
+                if(result === true){
+
+                    const arr = foundUser.email.split(" ");
+                    const userName = (arr[0]);
+
+                    Post.find({},function(err,foundItems){
+                        res.render("login_home",{
+                            fullUser: foundUser.email,
+                            userName: userName,
+                            homeContent: homeStartingContent,
+                            contents: foundItems
+                        });
+                
+                    });
+
+                } else {
+                    res.redirect("/incorrectPass");
+                }
+            });  
+
+        } else {
+            res.redirect("/userNotExists");
+        }
+    });
+    
+});
+
+app.get("/login_about",function(req,res){
+
+    const fullUser = req.query.fullUser;
+    const arr = fullUser.split(" ");
+    const userName = _.upperCase(arr[0]);
+
+    Post.find({},function(err,foundItems){
+        res.render("login_about",{
+            fullUser: fullUser,
+            userName: userName,
+            aboutContent: aboutContent
+        });
+
+    });
+
+});
+
+app.get("/compose",function(req,res){
+
+    const fullUser = req.query.fullUser;
+    const arr = fullUser.split(" ");
+    const userName = _.upperCase(arr[0])
+
+    Post.find({},function(err,foundItems){
+        res.render("compose",{
+            fullUser: fullUser,
+            userName: userName
+        });
+
+    });
+});
+
+
+app.post("/compose",function(req,res){
+
+    const fullUser = req.query.fullUser;
+    
+    const newPost = new Post({
+        name: _.lowerCase(req.body.titleBody),
+        body: req.body.postBody 
+    });
+    newPost.save(function(err){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/login_home?fullUser=" + fullUser);
+        }
+    });
+
+    
+
+});
+
+
 app.get("/login_post",function(req,res){
     const requestedPost = _.lowerCase(req.body.searchData);
 
@@ -155,187 +272,80 @@ app.get("/login_post",function(req,res){
 });
 
 
-
-app.get("/login_home",function(req,res){
-
-    userData.findOne({isLogin: true},function(err,foundEmail){
-        const arr = foundEmail.email.split(" ");
-        const userName = _.upperCase(arr[0]);
-
-        Post.find({},function(err,foundItems){
-            res.render("login_home",{
-                userName: userName,
-                homeContent: homeStartingContent,
-                contents: foundItems
-            });
-    
-        });
-    });
-    
-    
-    
-});
-
-app.post("/login_home",function(req,res){
-
-    const email = _.lowerCase(req.body.emailInput);
-    const password = req.body.passwordInput;
-
-    userData.findOne({email: email},function(err,foundEmail){
-        if(foundEmail){
-            
-            userData.findOne({email: email ,password: password},function(err,foundPass){
-                if(foundPass){
-
-                    userData.findOneAndUpdate({email: email},{isLogin: true},function(err,data){
-                        if(err){
-                            console.log(err);
-                        } else {
-                            console.log(data);
-                        }
-                    });
-
-                    const arr = foundEmail.email.split(" ");
-                    const userName = _.upperCase(arr[0]);
-
-                    Post.find({},function(err,foundItems){
-                        res.render("login_home",{
-                            userName: userName,
-                            homeContent: homeStartingContent,
-                            contents: foundItems
-                        });
-                
-                    });
-                    
-                } else {
-                    res.redirect("/incorrectPass");
-                }
-
-            });
-
-        } else {
-            res.redirect("/userNotExists");
-        }
-    });
-    
-});
-
-app.get("/login_about",function(req,res){
-
-    userData.findOne({isLogin: true},function(err,foundEmail){
-        const arr = foundEmail.email.split(" ");
-        const userName = _.upperCase(arr[0]);
-
-        Post.find({},function(err,foundItems){
-            res.render("login_about",{
-                userName: userName,
-                aboutContent: aboutContent
-            });
-    
-        });
-    });
-});
-
-app.get("/compose",function(req,res){
-
-    userData.findOne({isLogin: true},function(err,foundEmail){
-        const arr = foundEmail.email.split(" ");
-        const userName = _.upperCase(arr[0]);
-
-        Post.find({},function(err,foundItems){
-            res.render("compose",{
-                userName: userName
-            });
-    
-        });
-    });
-});
-
-
-app.post("/compose",function(req,res){
-    const newPost = new Post({
-        name: _.lowerCase(req.body.titleBody),
-        body: req.body.postBody 
-    });
-    newPost.save();
-
-    res.redirect("/login_home");
-
-});
+// for search box
 
 app.post("/post",function(req,res){
     const requestedPost = _.lowerCase(req.body.searchData);
+    const fullUser = req.query.fullUser;
     
-    userData.findOne({isLogin: true},function(err,foundEmail){
+    if(fullUser){
+        const arr = fullUser.split(" ");
+        const userName = _.upperCase(arr[0]);
 
-        if(foundEmail){
-            const arr = foundEmail.email.split(" ");
-            const userName = _.upperCase(arr[0]);
+        Post.findOne({name: requestedPost},function(err,post){
 
-            Post.findOne({name: requestedPost},function(err,post){
-
-                res.render("login_post",{
-                    userName: userName,
-                    postTitle: post.name,
-                    postBody: post.body
-                });
-            
+            res.render("login_post",{
+                fullUser: fullUser,
+                userName: userName,
+                postTitle: post.name,
+                postBody: post.body
             });
-
-        } else {
-            Post.findOne({name: requestedPost},function(err,post){
-
-                res.render("post",{
-                    postTitle: post.name,
-                    postBody: post.body
-                });
-
-            });
-
-        }
         
-    });
+        });
+
+    } else {
+        Post.findOne({name: requestedPost},function(err,post){
+
+            res.render("post",{
+                postTitle: post.name,
+                postBody: post.body
+            });
+
+        });
+
+    }
+
     
 
 });
 
-app.get("/posts/:postId",function(req,res){
+// for url
+
+app.get("/posts/:postId/fullUser/:fullUser",function(req,res){
     const requestedId = req.params.postId;
+    const fullUser = req.params.fullUser;
 
-    userData.findOne({isLogin: true},function(err,foundEmail){
+    if(fullUser){
+        const arr = fullUser.split(" ");
+        const userName = _.upperCase(arr[0]);
 
-        if(foundEmail){
-            const arr = foundEmail.email.split(" ");
-            const userName = _.upperCase(arr[0]);
+        Post.findOne({_id: requestedId},function(err,post){
 
-            Post.findOne({_id: requestedId},function(err,post){
-
-                res.render("login_post",{
-                    userName: userName,
-                    postTitle: post.name,
-                    postBody: post.body
-                });
-            
+            res.render("login_post",{
+                fullUser: fullUser,
+                userName: userName,
+                postTitle: post.name,
+                postBody: post.body
             });
-
-        } else {
-            Post.findOne({_id: requestedId},function(err,post){
-
-                res.render("post",{
-                    postTitle: post.name,
-                    postBody: post.body
-                });
-
-            });
-
-        }
         
-    });
+        });
+
+    } else {
+        Post.findOne({_id: requestedId},function(err,post){
+
+            res.render("post",{
+                postTitle: post.name,
+                postBody: post.body
+            });
+
+        });
+
+    }
     
 
 
 });
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started running succesfully.");
 });
